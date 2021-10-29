fd7128300fe9ca56a4fc071dac394bf8c73acfa9
Artem1KN <91216403+Artem1KN@users.noreply.github.com>
2021-10-29 22:35:08 +0500

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int N, i = 1, sum = 0;
+	cin >> N;
+	while (i <= N)
+	{
+		sum += i * i;
+		i += 1;
+	}
+	return sum;
+	cout << sum;
+}
---
+++ 16.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	double m, t;
+	cout << "N";
+	cin >> N;
+	cout << "m";
+	cin >> m;
+	for (int i = 2; i < -N; i += 1)
+	{
+		m + -1 / 6.;
+		t += m;
+
+	}
+	cout << t;
+}
