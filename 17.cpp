e1d6fe4c262a966343269cf2dbb372e12d1a4405
Artem1KN <91216403+Artem1KN@users.noreply.github.com>
2021-10-29 22:31:33 +0500

    Add files via upload

---
+++ 17.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int h, x, sum = 0;
+    cout << "number of students";
+    cin >> x;
+    for (int i = 1; i <= x; i+=1)
+    {
+        cout << "height" << i;
+        cin >> h;
+        sum += h;
+    }
+    cout << sum / x;
+}
